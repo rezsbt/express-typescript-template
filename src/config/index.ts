e3567bac66f39dc242e3472626bffb88056526ce
Reza Sabet <rezsabet24@gmail.com>
2025-02-08 14:29:08 +0330

    Add environments (development, production and test)

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
 import { config } from 'dotenv'
-config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` })
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` })
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true'
 export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN, HOST, SWAGGER_TITLE, SWAGGER_DESCRIPTION, SWAGGER_VERSION } = process.env
