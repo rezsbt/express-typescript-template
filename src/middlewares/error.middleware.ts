501ed37df449d801f2eda41ed5a31199d19655e0
Reza <55884874+rezsbt@users.noreply.github.com>
2025-02-12 17:46:49 +0330

    Show error details in error middleware

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -8,6 +8,11 @@ export const ErrorMiddleware = (error: HttpException, req: Request, res: Respons
     const message: string = error.message || 'Something went wrong'
 
     logger.error(`[${req.method}] ${req.path} >> StatusCode:: ${status}, Message:: ${message}`)
+    console.log('~~~~~~~~~~~~~~~~~~~~ Error ~~~~~~~~~~~~~~~~~~~~')
+    console.log('Name: ', error.name)
+    console.log('Message: ', error.message)
+    console.log('Stack: ', error.stack)
+    console.log('~~~~~~~~~~~~~~~~~~~~ Error ~~~~~~~~~~~~~~~~~~~~')
     res.status(status).json({ message })
   } catch (error) {
     next(error)
