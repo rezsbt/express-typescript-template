f6eb7ad947116e2196a0460f97238682ca9a82e5
Reza Sabet <rezsabet24@gmail.com>
2025-02-14 00:45:38 +0330

    Add app version configuration and clean up environment validation

--- src/utils/validateEnv.ts
+++ src/utils/validateEnv.ts
@@ -2,15 +2,11 @@ import { cleanEnv, port, str } from 'envalid'
 
 export const ValidateEnv = () => {
   cleanEnv(process.env, {
-    // ** Environment
     NODE_ENV: str(),
-    // ** Host & Port
     PORT: port(),
     HOST: str(),
-    // ** Swagger
     SWAGGER_TITLE: str(),
     SWAGGER_DESCRIPTION: str(),
-    // ** Database
     DATABASE_URI: str(),
     DATABASE_NAME: str()
   })
